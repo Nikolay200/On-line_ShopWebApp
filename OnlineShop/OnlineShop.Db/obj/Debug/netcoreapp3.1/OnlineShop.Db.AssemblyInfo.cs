69b66733cea3162b6f896a67a2020a89edee678b
Nikolay Smirnov <nikolay.smirnov@autogramma.ru>
2025-03-14 10:26:24 +0300

    Add file .gitignore

--- OnlineShop/OnlineShop.Db/obj/Debug/netcoreapp3.1/OnlineShop.Db.AssemblyInfo.cs
+++ OnlineShop/OnlineShop.Db/obj/Debug/netcoreapp3.1/OnlineShop.Db.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop.Db")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f67d59144868ec792f9f7aa4381bf8df819bdab")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop.Db")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop.Db")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
